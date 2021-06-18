38a80e8d7c517a3cdcaa8aeef473a84374de46a3
alissonrangel <alisson.rangel@hotmail.com>
2021-06-18 14:09:00 -0300

    aulas 4 e 5, controle das areas da tela e preenchendo a interface com numeros

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+  {
+      titulo: 'VEREADOR',
+      numeros: 5,
+      candidatos: [
+          {
+              numero: 38111,
+              nome: 'Fulano de Tal',
+              partido: 'ABC',
+              fotos: [
+                  {url: '1.png', legenda: 'Vereador', small: false}
+              ]
+          },
+          {
+              numero: 77222,
+              nome: 'Beltrano da Silva',
+              partido: 'DEFG',
+              fotos: [
+                  {url: '2.png',legenda: 'Vereador',small: false}
+              ]
+          }
+      ]
+  },
+  {
+      titulo: 'PREFEITO',
+      numeros: 2,
+      candidatos: [
+          {
+              numero: 99,
+              nome: 'Ciclano',
+              partido: 'ABC',
+              vice: 'Cic',
+              fotos: [
+                  {url: '3.png', legenda: 'Prefeito', small: false},
+                  {url: '4.png', legenda: 'Vice-Prefeito', small: true}
+              ]
+          },
+          {
+              numero: 84,
+              nome: 'Zulano',
+              partido: 'QWERTY',
+              vice: 'Zul',
+              fotos: [
+                  {url: '1.png', legenda: 'Prefeito', small: false},
+                  {url: '2.png', legenda: 'Vice-Prefeito', small: true}
+              ]
+          }
+      ]
+  }
+];
